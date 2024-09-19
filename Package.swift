f980eaaaf61c8aafaae9e8db5bd46d9fddaeb28b
Jaehong Kang <sinoru@me.com>
2024-09-19 11:25:10 +0900

    Renaming module

--- Package.swift
+++ Package.swift
@@ -8,17 +8,17 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "swift-dave",
-            targets: ["swift-dave"]),
+            name: "DAVE",
+            targets: ["DAVE"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-dave"),
+            name: "DAVE"),
         .testTarget(
-            name: "swift-daveTests",
-            dependencies: ["swift-dave"]
+            name: "DAVETests",
+            dependencies: ["DAVE"]
         ),
     ]
 )
